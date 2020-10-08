af0e57c3e6f5f7faa4a21b3082680134394752a8
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-07 18:04:58 -0700

    update server and testing api

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const SchemeRouter = require("./schemes/scheme-router.js");
 const server = express();
 
 server.use(express.json());
-server.use("/api/schemes", SchemeRouter);
-
+server.use("/schemes", SchemeRouter);
+server.get("/schemes", (req, res, next ) =>{
+    res.status(200).json({ api: "testing for Web34 Multi-table Queries  ", query: req.query })
+    next();
+});
 
 
 module.exports = server;
