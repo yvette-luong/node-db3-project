d021f8713f0a871c717e858c23e280b7c1b8ca36
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-07 17:46:17 -0700

    update schema

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,17 +1,67 @@
+const { insert } = require("../knexconfig");
 const database = require("../knexconfig");
 
 function find() {
   return database("schemes");
 }
 
-function findById(id) {}
+function findById(id) {
+    return database("schemes")
+    .where({id})
+    .then((scheme) =>{
+        if (!scheme.length) {
+            return null;
+        }else{
+            return scheme
+        }
+    })
+}
 
-function findSteps(id) {}
+function findSteps(id) {
+   //have to join `steps` table, 
+   return database("schemes")
+   .join("steps", "steps.scheme_id", "scheme.id")
+   .select(
+       "steps.id",
+       "steps.step_number",
+       "steps.instructions", 
+       "shemes.scheme_name"
+    )
+    .where({"schemes.id":id})
+    .orderBy("steps.step_number")
+}
 
-function add(scheme) {}
+function add(scheme) {
+    return database("scheme")
+    .insert(scheme) //Inserts scheme into the database
+    .then((id)=>{ // Resolves to the newly inserted scheme, including id.
+        return findById(id[0]);
+    })
+    .catch((err) =>{
+        return null;
+    });
+}
 
-function update(changes, id) {}
+function update(changes, id) {
+    database("scheme")
+        .where({id})
+        .update(changes)
+        .then((scheme) =>{
+            if (scheme) {
+                return findById(id)
+            } else {
+                return null;
+            }
+        })
+        .catch((err) =>{
+            return null;
+        })
+}
 
-function remove(id) {}
+function remove(id) {
+    return database ("schemes")
+    .where({ id }).delete();
+
+}
 
 module.exports = { find, findById, findSteps, add, update, remove };
--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require("express");
 
 const SchemeRouter = require("./schemes/scheme-router.js");
-const db = require("./knexconfig");
+
 const server = express();
 
 server.use(express.json());
 server.use("/api/schemes", SchemeRouter);
 
-server.get("/api/posts", (req, res) => {
-  db("posts")
-    .then((posts) => {})
-    .catch((error) => {
-      res.status(500).json({ message: error.message });
-    });
-});
+
+
 module.exports = server;
