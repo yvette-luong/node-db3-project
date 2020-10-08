27fd6f34fa58f98a1f5e21433e125e85f6bea013
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-07 17:12:40 -0700

    set up schema-model layout and update server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const SchemeRouter = require('./schemes/scheme-router.js');
-
+const SchemeRouter = require("./schemes/scheme-router.js");
+const db = require("./knexconfig");
 const server = express();
 
 server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+server.use("/api/schemes", SchemeRouter);
 
+server.get("/api/posts", (req, res) => {
+  db("posts")
+    .then((posts) => {})
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
+    });
+});
 module.exports = server;
